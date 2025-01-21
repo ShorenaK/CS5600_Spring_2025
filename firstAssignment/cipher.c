e78b8b265e4b35adf6a99c3ec0c18ef418670c46
Shorena K. Anzhilov <shorenaanzhilov@gmail.com>
2025-01-21 13:48:38 -0500

    cipher.c file cteated

---
+++ firstAssignment/cipher.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "polybius.h"
+
+// name: Shorena K. Anzhilov
+// email: anzhilov.s@northeastern.edu
+// Spring, 2025
+
+
+#define MAX_TEXT_LENGTH 256
+
+void printUsage(const char *programName) {
+    printf("Usage: %s -e \"text to encrypt\"\n", programName);
+    printf("       %s -d \"encrypted text\"\n", programName);
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        printUsage(argv[0]);
+        return 1;
+    }
+
+    char result[MAX_TEXT_LENGTH];
+
+    if (strcmp(argv[1], "-e") == 0) {
+        pbEncode(argv[2], result);
+        printf("Encoded: %s\n", result);
+    } else if (strcmp(argv[1], "-d") == 0) {
+        pbDecode(argv[2], result);
+        printf("Decoded: %s\n", result);
+    } else {
+        printUsage(argv[0]);
+        return 1;
+    }
+
+    return 0;
+}
+
