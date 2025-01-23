60f1925cacb2ee854665eb24965b17560f9ea848
Shorena K. Anzhilov <100386342+ShorenaK@users.noreply.github.com>
2025-01-23 17:17:20 -0500

    Update queue.h

--- week5_Assignment/queue.h
+++ week5_Assignment/queue.h
@@ -2,9 +2,13 @@
 #include <stdlib.h>
 #include <string.h>
 
-// name: Shorena K. Anzhilov
-// email: anzhilov.s@northeastern.edu
-// Spring, 2025
+/*
+* queue.h / Practice Assignment / Program Execution and Process Queue
+*
+* Shorena K. Anzhilov / CS5600 / Northeastern University
+* Spring 2025 / Jan , 2025 
+*
+*/
 
 #ifndef QUEUE_H
 #define QUEUE_H
