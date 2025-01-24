3f996eeba2e07e6aafb42d5da36e61f5976897ae
Shorena K. Anzhilov <100386342+ShorenaK@users.noreply.github.com>
2025-01-24 18:37:11 -0500

    Update queuetest.c

--- week5_Assignment/queuetest.c
+++ week5_Assignment/queuetest.c
@@ -6,7 +6,7 @@
 * queuetest.c / Practice Assignment / Program Execution and Process Queue
 *
 * Shorena K. Anzhilov / CS5600 / Northeastern University
-* Spring 2025 / Jan , 2025 
+* Spring 2025 / Feb , 2025 
 *
 */
 
