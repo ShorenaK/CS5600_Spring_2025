bef414650149bf625b31d64843c0813012af990d
Shorena K. Anzhilov <shorenaanzhilov@gmail.com>
2025-01-24 15:43:12 -0500

    new updated files

--- week6_Assignment/cipher.c
+++ week6_Assignment/cipher.c
@@ -11,20 +11,42 @@
 *
 */
 
+//  This program handles command-line input and performs encryption
+// or decryption using the Polybius Square cipher.
+
 #define MAX_TEXT_LENGTH 256
 
-int main(int argc, char *argv[]) {
-	(void)argc;
-	(void)argv;
-
-    	char input[MAX_TEXT_LENGTH];
-
-    while (fgets(input, sizeof(input), stdin)) {
-        char result[MAX_TEXT_LENGTH];
-        pbEncode(input, result);
-        printf("%s\n", result);
-    }
-    
-    return 0;
+void printUsage(void) {
+    printf("Usage: cipher -e to encrypt or cipher -d to decrypt\n");
 }
 
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        void printUsage(void);
+        return 1;
+    }
+
+    char input[MAX_TEXT_LENGTH];
+    char result[MAX_TEXT_LENGTH];
+
+    // Check for encryption or decryption mode
+    if (strcmp(argv[1], "-e") == 0) {
+        // Encoding mode
+        while (fgets(input, sizeof(input), stdin)) {
+            input[strcspn(input, "\n")] = 0; 
+            pbEncode(input, result);
+            printf("%s\n", result);
+ 	}
+    } else if (strcmp(argv[1], "-d") == 0) {
+        // Decoding mode
+        while (fgets(input, sizeof(input), stdin)) {
+            input[strcspn(input, "\n")] = 0; 
+            pbDecode(input, result);
+            printf("%s\n", result);
+        }
+    } else {
+        void printUsage(void);
+        return 1;
+    }
+    return 0;
+}
