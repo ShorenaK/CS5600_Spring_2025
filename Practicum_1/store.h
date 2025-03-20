2746bbf1d292baf740e5ce58790143642dca8592
Shorena K. Anzhilov <shorenaanzhilov@gmail.com>
2025-03-20 16:21:14 -0400

    store.h created and done

---
+++ Practicum_1/store.h
@@ -0,0 +1,22 @@
+#ifndef STORAGE_H
+#define STORAGE_H
+
+#include "message.h"
+
+#define MSG_FILE "messages.dat"
+
+/*
+* store.h / Practicum 1
+*
+* Shorena K. Anzhilov / CS5600 / Northeastern University
+* Spring 2025 / March, 2025
+*
+*/
+
+
+// Function declarations for storeing and retrieving messages.
+void store_msg(Message* msg);
+Message* retrieve_msg(int id);
+
+#endif
+
