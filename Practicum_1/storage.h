65313c03db222576ce2ecf3b0c27d7e6cb2d0f5e
Shorena K. Anzhilov <shorenaanzhilov@gmail.com>
2025-03-22 16:12:56 -0400

    cache

--- Practicum_1/storage.h
+++ Practicum_1/storage.h
@@ -4,6 +4,8 @@
 #include "message.h"
 
 #define MSG_FILE "messages.dat"
+// Number of messages stored in cache
+#define CACHE_SIZE 16 
 
 /*
 * storage.h / Practicum 1
@@ -17,6 +19,12 @@
 // Function declarations for storeing and retrieving messages.
 int store_msg(Message* msg);
 Message* retrieve_msg(int id);
+
+// Cache functions, intializes the cache, searchines for a message in the cache,
+// and adds a new message to the cache.
+void init_cache();
+Message* cache_lookup(int id);
+void cache_insert(Message* msg);
 
 #endif
 
