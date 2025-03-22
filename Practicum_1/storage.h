9a62026d0fb2b94095dae367e72e7b2056125a41
Shorena K. Anzhilov <shorenaanzhilov@gmail.com>
2025-03-22 17:04:05 -0400

    cache replacement policies added

--- Practicum_1/storage.h
+++ Practicum_1/storage.h
@@ -4,17 +4,24 @@
 #include "message.h"
 
 #define MSG_FILE "messages.dat"
-// Number of messages stored in cache
+// Number of messages stored in cache, that can store 16 messages
 #define CACHE_SIZE 16 
 
 /*
 * storage.h / Practicum 1
 *
 * Shorena K. Anzhilov / CS5600 / Northeastern University
-* Spring 2025 / March, 2025
+* Spring,  March, 2025
 *
 */
 
+// Cache Replacement Policies
+typedef enum {
+    RANDOM_REPLACEMENT,
+    LRU_REPLACEMENT
+} CacheReplacementPolicy;
+
+extern CacheReplacementPolicy cache_replacement_policy;  // Global variable for policy
 
 // Function declarations for storeing and retrieving messages.
 int store_msg(Message* msg);
@@ -27,4 +34,3 @@ Message* cache_lookup(int id);
 void cache_insert(Message* msg);
 
 #endif
-
